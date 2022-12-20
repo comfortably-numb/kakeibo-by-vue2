66aee910be775dd8590d7906ab68dc58ffefe219
shintarou.chou <shintarou.chou@gmail.com>
2022-12-20 16:01:23 +0900

    添加404页面路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,11 @@ import Home from "@/views/Home.vue";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter);
 
-const routes: Array<RouteConfig> = [
+const routes = [
   {
     path: "/",
     redirect: "/money",
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
   {
     path: "/statistics",
     component: Statistics,
+  },
+  {
+    path: "*",
+    component: NotFound,
   },
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>当前页面不存在</div>
+    <div><router-link to="/">返回首页</router-link></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "NotFound",
+});
+</script>
+
+<style scoped></style>
