39cf3873c50599310b6aed666068c2bf54dd427d
shintarou.chou <shintarou.chou@gmail.com>
2022-12-20 17:26:13 +0900

    用slot优化三个页面的布局代码

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    Labels
-    <Nav />
+    <Layout>Labels</Layout>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  name: "Labels",
+});
 </script>
-
-<style scoped></style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    Statistics
-    <Nav />
+    <Layout>Statistics</Layout>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  name: "Statistics",
+});
 </script>
-
-<style scoped></style>
