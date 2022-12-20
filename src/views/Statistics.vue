95e469b7c33a1978d48754c581fe975a5f028239
shintarou.chou <shintarou.chou@gmail.com>
2022-12-20 14:49:21 +0900

    创建导航栏和页面雏形

---
+++ src/views/Statistics.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Statistics</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Statistics from "@/views/Labels.vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
