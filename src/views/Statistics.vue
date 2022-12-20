da6ec93911b87c37e5be0b750d5b85b76f7d48ff
shintarou.chou <shintarou.chou@gmail.com>
2022-12-20 15:30:39 +0900

    导航栏全局组件化

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>Statistics</div>
+  <div>
+    Statistics
+    <Nav />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Statistics from "@/views/Labels.vue";
 
 export default Vue.extend({});
 </script>
