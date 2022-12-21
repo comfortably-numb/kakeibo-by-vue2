2904e7a9545349b3e843fb816ef970cda28b2528
shintarou.chou <shintarou.chou@gmail.com>
2022-12-21 11:52:16 +0900

    调整导航栏样式 更换icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,47 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">记账</router-link>
-    <Icon name="money" />
-    |
-    <router-link to="/labels">标签</router-link>
-    <Icon name="labels" />
-
-    |
-    <router-link to="/statistics">统计</router-link>
-    <Icon name="statistics" />
-  </div>
+  <nav>
+    <router-link to="/money" class="nav-item" active-class="selected">
+      <Icon name="money" />
+      记账
+    </router-link>
+    <router-link to="/labels" class="nav-item" active-class="selected">
+      <Icon name="labels" />
+      标签
+    </router-link>
+    <router-link to="/statistics" class="nav-item" active-class="selected">
+      <Icon name="statistics" />
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Icon from "@/components/Icon.vue";
 
 export default Vue.extend({});
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  flex-direction: row;
+  font-size: 12px;
+  > .nav-item {
+    padding: 2px 0;
+    width: 33.3333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon {
+      width: 30px;
+      height: 30px;
+    }
+  }
+  > .nav-item.selected {
+    color: purple;
+  }
 }
 </style>
