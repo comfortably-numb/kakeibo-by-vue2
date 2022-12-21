d9f8bf2020acda90a2d16f669411d862201da9f5
shintarou.chou <shintarou.chou@gmail.com>
2022-12-21 11:21:57 +0900

    icon组件化

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,33 +1,19 @@
 <template>
   <div class="nav">
     <router-link to="/money">记账</router-link>
-    <svg>
-      <use xlink:href="#money" />
-    </svg>
+    <Icon name="money" />
     |
     <router-link to="/labels">标签</router-link>
-    <svg>
-      <use xlink:href="#labels" />
-    </svg>
+    <Icon name="labels" />
 
     |
     <router-link to="/statistics">统计</router-link>
-    <svg>
-      <use xlink:href="#statistics" />
-    </svg>
+    <Icon name="statistics" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-
-let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
-  requireContext.keys().forEach(requireContext);
-try {
-  importAll(require.context("../assets/icons", true, /\.svg$/));
-} catch (error) {
-  console.log(error);
-}
 
 export default Vue.extend({});
 </script>
