bf559a08cf6d43071ed38d1275b46415c6c70294
shintarou.chou <shintarou.chou@gmail.com>
2022-12-21 10:59:23 +0900

    引入svgloader添加icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="nav">
     <router-link to="/money">记账</router-link>
+    <svg>
+      <use xlink:href="#money" />
+    </svg>
     |
     <router-link to="/labels">标签</router-link>
+    <svg>
+      <use xlink:href="#labels" />
+    </svg>
+
     |
     <router-link to="/statistics">统计</router-link>
+    <svg>
+      <use xlink:href="#statistics" />
+    </svg>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
 
 export default Vue.extend({});
 </script>
