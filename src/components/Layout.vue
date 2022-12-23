b98a076b4a2f56426e4a0b8c13f7b99237891342
shintarou.chou <shintarou.chou@gmail.com>
2022-12-23 15:47:09 +0900

    调整money画面ui

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`   ">
       <slot />
     </div>
     <Nav />
@@ -11,19 +11,19 @@
 import Vue from "vue";
 
 export default Vue.extend({
+  props: ['classPrefix'],
   name: "Layout",
 });
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content {
-  overflow: auto;
-  flex-grow: 1;
-}
+  .layout-wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content {
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,29 +1,6 @@
 <template>
   <div>
-    <Layout>
-      <div class="tags">
-        <ul class="current">
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ul>
-        <div class="new">
-          <button>新增标签</button>
-        </div>
-      </div>
-      <div>
-        <label class="notes">
-          <span class="name">备注</span>
-          <input type="text" placeholder="输入备注" />
-        </label>
-      </div>
-      <div>
-        <ul class="types">
-          <li class="selected">支出</li>
-          <li>收入</li>
-        </ul>
-      </div>
+    <Layout class-prefix="layout">
       <div class="number-pad">
         <div class="output">100</div>
         <div class="buttons">
@@ -38,11 +15,37 @@
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
+          <button class="ok">OK</button>
+          <button class="zero">0</button>
           <button>.</button>
         </div>
       </div>
+      <div>
+        <ul class="types">
+          <li class="selected">支出</li>
+          <li>收入</li>
+        </ul>
+      </div>
+      <div>
+        <label class="notes">
+          <span class="name">备注</span>
+          <input type="text" placeholder="输入备注" />
+        </label>
+      </div>
+      <div class="tags">
+        <div class="new">
+          <button>新增标签</button>
+        </div>
+        <ul class="current">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+      </div>
+
+
+
     </Layout>
   </div>
 </template>
@@ -55,13 +58,26 @@ export default Vue.extend({
 });
 </script>
 
+<style lang="scss">
+
+.layout-content{
+  border:3px solid red;
+  display: flex;
+  flex-direction: column-reverse
+}
+</style>
+
 <style lang="scss" scoped>
 @import "@/assets/style/helper.scss";
 .tags {
   font-size: 14px;
   padding: 16px;
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column-reverse;
   > .current {
     display: flex;
+    flex-wrap: wrap;
     > li {
       background: #d9d9d9;
       height: 24px;
@@ -69,6 +85,7 @@ export default Vue.extend({
       border-radius: (24px/2);
       padding: 0 16px;
       margin-right: 12px;
+      margin-top: 4px;
     }
   }
 
@@ -123,9 +140,54 @@ export default Vue.extend({
     }
   }
 }
-.number-pad{
-  .output{
-
+.number-pad {
+  
+    .output {
+      @extend %clearFix;
+      @extend %innerShadow;
+      font-size: 36px;
+      font-family: Consolas, monospace;
+      padding: 9px 16px;
+      text-align: right;
+    }
+    .buttons {
+      @extend %clearFix;
+      flex-wrap: wrap;
+       > button{
+        width: 25%;
+        height: 64px;
+        background: transparent;
+        border: none;
+        &.ok{
+          height: 64*2px;
+          float: right;
+        }
+        &.zero{
+          width: 25*2%;
+        }
+        $bg:#f2f2f2;
+        &:nth-child(1){
+          background: $bg;
+        }
+        &:nth-child(2),&:nth-child(5){
+          background: darken($color: $bg, $amount: 4%);
+        }
+        &:nth-child(3),&:nth-child(6),&:nth-child(9){
+          background: darken($color: $bg, $amount: 4*2%);
+        }
+        &:nth-child(4),&:nth-child(7),&:nth-child(10){
+          background: darken($color: $bg, $amount: 4*3%);
+        }
+        &:nth-child(8),&:nth-child(11),&:nth-child(13){
+          background: darken($color: $bg, $amount: 4*4%);
+        }
+        &:nth-child(14){
+          background: darken($color: $bg, $amount: 4*5%);
+        }
+        &:nth-child(12){
+          background: darken($color: $bg, $amount: 4*6%);
+        }
+       }
+    }
   }
-}
 </style>
