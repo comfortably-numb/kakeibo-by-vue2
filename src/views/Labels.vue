de434b791ed5b0fb8e98896555d97a7432c66ab7
shintaro.cho <shintarou.chou@gmail.com>
2022-12-30 20:36:38 +0900

    暂存

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    <Layout>Labels</Layout>
+    <Layout>
+      <ol class="tags">
+        <li><span>衣</span>
+          <Icon name="right"></Icon>
+        </li>
+        <li><span>食</span>
+          <Icon name="right"></Icon>
+        </li>
+        <li><span>住</span>
+          <Icon name="right"></Icon>
+        </li>
+        <li><span>行</span>
+          <Icon name="right"></Icon>
+        </li>
+      </ol>
+      <div>
+        <button>
+          新建标签
+        </button>
+      </div>
+    </Layout>
   </div>
 </template>
 
@@ -11,3 +31,19 @@ export default Vue.extend({
   name: "Labels",
 });
 </script>
+
+<style lang="scss" scoped>
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+  }
+}
+</style>
