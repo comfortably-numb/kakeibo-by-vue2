e496e33378d87b030ad2d43f0cb14d08233ef93a
shintarou.chou <shintarou.chou@gmail.com>
2023-02-19 17:54:41 +0900

    FormItem中的value变更为prop

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -2,7 +2,7 @@
   <div>
     <label class="formItem">
       <span class="name">{{ this.fieldName }}</span>
-      <input type="text" v-model="value" :placeholder="this.placeholder"/>
+      <input type="text" :value="value" @input="onNotesChanged($event.target.value)" :placeholder="this.placeholder"/>
     </label>
   </div>
 
@@ -14,8 +14,7 @@ import {Component, Prop, Watch} from "vue-property-decorator";
 
 @Component
 export default class FormItem extends Vue {
-  value = "";
-
+  @Prop({default: ""}) value!: string;
   @Prop({required: true}) fieldName!: string;
   @Prop() placeholder?: string;
 
